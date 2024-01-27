9c20c7a3507d85e288b44688d19f01d10d2997ae
“CraftyVlad“ <vladsemboss@gmail.com>
2024-01-27 11:35:55 +0200

    made addition

--- CalculatorHW/CalculatorHW.cpp
+++ CalculatorHW/CalculatorHW.cpp
@@ -6,28 +6,9 @@ int Add(int a, int b) {
     return a + b;
 }
 
-int Subtract(int a, int b) {
-    return a - b;
-}
-
-int Multiply(int a, int b) {
-    return a * b;
-}
-
-double Divide(double a, double b) {
-    if (b == 0) {
-        cout << "Error: division by zero!" << endl;
-        return 0.0;
-    }
-    return a / b;
-}
-
 int main() {
     double a = 10, b = 5;
     cout << "a + b = " << Add(a, b) << endl;
-    cout << "a - b = " << Subtract(a, b) << endl;
-    cout << "a * b = " << Multiply(a, b) << endl;
-    cout << "a / b = " << Divide(a, b) << endl;
 
     return 0;
 }
