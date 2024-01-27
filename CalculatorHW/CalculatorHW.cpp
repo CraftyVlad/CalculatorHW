70de11abf08207393eb205c854a4997a1925a960
“CraftyVlad“ <vladsemboss@gmail.com>
2024-01-27 11:30:13 +0200

    made addition

--- CalculatorHW/CalculatorHW.cpp
+++ CalculatorHW/CalculatorHW.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+int Add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    double a = 10, b = 5;
+    cout << "a + b = " << Add(a, b) << endl;
+
+    return 0;
 }
