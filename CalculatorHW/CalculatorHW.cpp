ed2b1a492f48d9b98e81920b408d1abc0a9b94cf
“CraftyVlad“ <vladsemboss@gmail.com>
2024-01-27 11:31:39 +0200

    made multiplication

--- CalculatorHW/CalculatorHW.cpp
+++ CalculatorHW/CalculatorHW.cpp
@@ -10,10 +10,16 @@ int Subtract(int a, int b) {
     return a - b;
 }
 
+int Multiply(int a, int b) {
+    return a * b;
+}
+
+
 int main() {
     double a = 10, b = 5;
     cout << "a + b = " << Add(a, b) << endl;
     cout << "a - b = " << Subtract(a, b) << endl;
+    cout << "a * b = " << Multiply(a, b) << endl;
 
     return 0;
 }
