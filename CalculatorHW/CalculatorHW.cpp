a07dcf297105196b5a527efd97be33fd444031da
“CraftyVlad“ <vladsemboss@gmail.com>
2024-01-27 11:37:05 +0200

    made division

--- CalculatorHW/CalculatorHW.cpp
+++ CalculatorHW/CalculatorHW.cpp
@@ -14,11 +14,20 @@ int Multiply(int a, int b) {
     return a * b;
 }
 
+double Divide(double a, double b) {
+    if (b == 0) {
+        cout << "Error: division by zero!" << endl;
+        return 0.0;
+    }
+    return a / b;
+}
+
 int main() {
     double a = 10, b = 5;
     cout << "a + b = " << Add(a, b) << endl;
     cout << "a - b = " << Subtract(a, b) << endl;
     cout << "a * b = " << Multiply(a, b) << endl;
+    cout << "a / b = " << Divide(a, b) << endl;
 
     return 0;
 }
