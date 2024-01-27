c877c7535ddcd7c96ac175b8b48f98002aaa2920
“CraftyVlad“ <vladsemboss@gmail.com>
2024-01-27 11:36:20 +0200

    made subtraction

--- CalculatorHW/CalculatorHW.cpp
+++ CalculatorHW/CalculatorHW.cpp
@@ -6,9 +6,14 @@ int Add(int a, int b) {
     return a + b;
 }
 
+int Subtract(int a, int b) {
+    return a - b;
+}
+
 int main() {
     double a = 10, b = 5;
     cout << "a + b = " << Add(a, b) << endl;
+    cout << "a - b = " << Subtract(a, b) << endl;
 
     return 0;
 }
